1c31f956e69cc355dc03a81b2e4f6fd997c92263
Peter Muturi <pmuturi65@gmail.com>
2015-08-28 09:36:28 +0300

    price

--- db/migrate/20150828063354_create_products.rb
+++ db/migrate/20150828063354_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :title
-      t.decimal :price
+      t.decimal :price, precision: 8, scale: 2
       t.text :description
       t.string :image_url
 
